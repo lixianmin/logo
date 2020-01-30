dcc204d0e69a5e17e1e6ead006b97dddc0a33c8e
lixianmin <lixianmin@live.cn>
2020-01-30 13:14:41 +0800

    测试RollingFileAppender

--- logger_test.go
+++ logger_test.go
@@ -24,3 +24,19 @@ func TestConsoleAppender(t *testing.T) {
 
 	_ = l.Close()
 }
+
+func TestRollingFileAppender(t *testing.T) {
+	var l = NewLogger()
+
+	const flag = FlagDate | FlagTime | FlagShortFile | FlagLevel
+	l.SetFuncCallDepth(2)
+
+	var console = NewRollingFileAppender(flag, LevelInfo, "logs", "log_", 16)
+	l.AddAppender(console)
+
+	l.Info("This is info")
+	l.Warn("I am a warning")
+	l.Error("Error occurred")
+
+	_ = l.Close()
+}
