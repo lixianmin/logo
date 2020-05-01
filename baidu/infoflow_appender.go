e9480ec5566118bf7b1e5ec9696d7a7f344b8ac8
lixianmin <lixianmin@live.cn>
2020-05-02 00:04:09 +0800

    加入infoflow_appender

---
+++ baidu/infoflow_appender.go
@@ -0,0 +1,66 @@
+package baidu
+
+import (
+	"fmt"
+	"github.com/lixianmin/logo"
+	"path"
+)
+
+/********************************************************************
+created:    2020-05-01
+author:     lixianmin
+
+Copyright (C) - All Rights Reserved
+*********************************************************************/
+
+// 这里没有选择使用InfoFlowArgs，是为了给infoflow.go中的InfoFlow类留出未来
+type InfoFlowAppenderArgs struct {
+	Talker      *InfoFlow
+	LevelFilter int
+}
+
+type InfoFlowAppender struct {
+	args InfoFlowAppenderArgs
+}
+
+func NewInfoFlowAppender(args InfoFlowAppenderArgs) *InfoFlowAppender {
+	checkInfoFlowAppenderArgs(&args)
+
+	var my = &InfoFlowAppender{
+		args: args,
+	}
+
+	return my
+}
+
+func (my *InfoFlowAppender) Write(message logo.Message) {
+	var level = message.GetLevel()
+	var args = my.args
+	if level < args.LevelFilter {
+		return
+	}
+
+	var filePath = message.GetFilePath()
+	var lineNum = message.GetLineNum()
+	var text = fmt.Sprintf("[%s:%d] %s", path.Base(filePath), lineNum, message.GetText())
+
+	var talker = args.Talker
+	switch level {
+	case logo.LevelInfo:
+		talker.SendInfo("", text)
+	case logo.LevelWarn:
+		talker.SendWarn("", text)
+	case logo.LevelError:
+		talker.SendError("", text)
+	}
+}
+
+func checkInfoFlowAppenderArgs(args *InfoFlowAppenderArgs) {
+	if args.Talker == nil {
+		panic("Talker should not be null")
+	}
+
+	if args.LevelFilter <= 0 {
+		args.LevelFilter = logo.LevelInfo
+	}
+}
