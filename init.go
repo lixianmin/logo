d7bbd308c2802fe859c130d1e7e05fb957a1f705
lixianmin <lixianmin@live.cn>
2020-07-31 16:53:42 +0800

    调整创建文件夹的权限

--- init.go
+++ init.go
@@ -7,6 +7,8 @@ author:     lixianmin
 Copyright (C) - All Rights Reserved
 *********************************************************************/
 
+const filePerm = 0770
+
 var theLogger = NewLogger()
 
 func init() {
